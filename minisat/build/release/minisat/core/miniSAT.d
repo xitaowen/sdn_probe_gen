19eb47b013633a053d2829085ea52723d81c1577
YBo <ybo2013@zju.edu.cn>
2015-03-23 19:36:10 +0800

    minisat added;

---
+++ minisat/build/release/minisat/core/miniSAT.d
@@ -0,0 +1,6 @@
+build/release/minisat/core/miniSAT.o: minisat/core/miniSAT.cc \
+ minisat/utils/System.h minisat/mtl/IntTypes.h minisat/utils/ParseUtils.h \
+ minisat/mtl/XAlloc.h minisat/utils/Options.h minisat/mtl/Vec.h \
+ minisat/core/Dimacs.h minisat/core/SolverTypes.h minisat/mtl/Alg.h \
+ minisat/mtl/IntMap.h minisat/mtl/Map.h minisat/mtl/Alloc.h \
+ minisat/core/Solver.h minisat/mtl/Heap.h
---
+++ minisat/test.py
@@ -0,0 +1,2 @@
+import miniSAT
+print miniSAT.solve("p cnf 100 3 1 2 0 -2 3 0 -3 0")
